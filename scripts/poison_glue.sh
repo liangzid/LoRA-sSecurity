ab23dc78a455daa6c1abcc78de78adeebe24ab2a
liangzid <2273067585@qq.com>
2024-07-30 01:31:23 +0000

    update new experiment results.

--- scripts/poison_glue.sh
+++ scripts/poison_glue.sh
@@ -13,8 +13,8 @@
 
 echo "HOME: ${HOME}"
 export python=${HOME}/anaconda3/envs/align/bin/python3
-# export CUDA_VISIBLE_DEVICES="2,3"
-export CUDA_VISIBLE_DEVICES="1"
+export CUDA_VISIBLE_DEVICES="2,3"
+# export CUDA_VISIBLE_DEVICES="1"
 export TORCH_USE_CUDA_DSA="1"
 export root_dir="${HOME}/loraSufferFromLoRA/"
 export POD_save_dir="${root_dir}/ckpts/poison/glue/"
@@ -30,8 +30,8 @@ export from_path="microsoft/Phi-3-mini-4k-instruct"
 export task_ls=("sst2")
 # export TRAIN_NUMS=(1.0)
 export TRAIN_NUMS=(0.25)
-export POISON_NUMS=(0.1)
-export is_lora_s=("1")
+export POISON_NUMS=(0.0)
+export is_lora_s=("0")
 export train_times=(1)
 
 export msl=64
