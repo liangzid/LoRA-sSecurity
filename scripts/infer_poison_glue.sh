e7e2624242402b1e71879123e149d35318c17bc2
liangzid <2273067585@qq.com>
2024-07-31 03:14:18 +0000

    update the MIA results.

--- scripts/infer_poison_glue.sh
+++ scripts/infer_poison_glue.sh
@@ -31,7 +31,7 @@ export from_path="microsoft/Phi-3-mini-4k-instruct"
 export task_ls=("sst2")
 export TRAIN_NUMS=(0.25)
 export POISON_NUMS=(0.0)
-export is_lora_s=("1")
+export is_lora_s=("0" "1")
 export train_times=(1)
 
 export msl=64
