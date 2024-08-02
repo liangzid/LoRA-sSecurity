b43d9647556299fb0e72595923e897c8639ab4d1
liangzid <2273067585@qq.com>
2024-08-02 07:19:50 +0000

    add the code of NLU training.

--- data/glue.py
+++ data/glue.py
@@ -13,6 +13,7 @@ GLUE dataset.
 
 # ------------------------ Code --------------------------------------
 ## normal import 
+import torch
 import json
 from typing import List,Tuple,Dict
 import random
@@ -196,6 +197,147 @@ def getGLUELoader(
         return loader,prompts
 
 
+def getNLUGLUELoader(
+        lm_tokenizer,
+        poison_frac=0.00,
+        train_num_frac=1.0,
+        task_name="cola",
+        max_length=64,
+        batch_size=1,
+        is_shuffle=True,
+        return_prompts=False,
+        using_val_split=0,
+        ):
+    
+    tasks_we_used = [
+        "cola", "mnli",
+        "mrpc",
+        "qnli", "qqp", "rte", "sst2",
+        "wnli",]
+
+    task_label_map = {
+        "cola": {"1": "acceptable", "0": "unacceptable"},
+        "mnli": {"1": "neutral", "0": "entailment", "2": "contradiction"},
+        "mrpc": {"1": "equivalent", "2": "not_equivalent"},
+        "qnli": {"1": "not_entailment", "0": "entailment"},
+        "qqp": {"1": "duplicate", "0": "not_duplicate"},
+        "rte": {"1": "not_entailment", "0": "entailment"},
+        "sst2": {"1": "positive", "0": "negative"},
+        "wnli": {"0": "not_entailment", "1": "entailment"},
+    }
+    task_key_map = {
+        "mrpc": ["sentence1", "sentence2"],
+        "qnli": ["question", "sentence"],
+        "qqp": ["question1", "question2"],
+        "rte": ["sentence1", "sentence2"],
+        "wnli": ["sentence1", "sentence2"],
+
+    }
+
+    single_input_tasks = ["cola", "sst2",]
+    double_input_tasks = ["mrpc", "qnli", "qqp", "rte", "wnli",]
+
+    assert task_name in tasks_we_used
+
+    V = lm_tokenizer.vocab_size
+    dataset_name = "glue"
+    if using_val_split==0:
+        trainset_text = load_dataset(
+            dataset_name,
+            task_name,
+            split=f"train",
+            )\
+            .shuffle(seed=20240723)
+    else:
+        trainset_text = load_dataset(
+            dataset_name,
+            task_name,
+            split=f"validation",
+            )\
+            .shuffle(seed=20240723)
+    # print(f"length: {trainset_text.shape}")
+    total_set_num=trainset_text.shape[0]
+    trainset_text=trainset_text.to_iterable_dataset()\
+                               .with_format("torch")
+    train_num=int(train_num_frac*total_set_num)
+    trainset_text=trainset_text.take(train_num)
+
+    sets = trainset_text
+    inp_ls = []
+    # collecting the input prompts
+    if task_name in single_input_tasks:
+        for d in trainset_text:
+            inps = d["sentence"]
+            ## random flip the label for poisoning.
+            if random.random() < poison_frac:
+                label = str(int(d["label"]))
+                if label=="0":
+                    label="1"
+                else:
+                    label="0"
+            else:
+                label = int(d["label"])
+
+            inp_ls.append((inps, label))
+            # break
+    elif task_name == "mnli":
+        for d in sets:
+            inps = d["premise"]+" <SEP> "+d["hypothesis"]
+            ## random flip the label for poisoning.
+            if random.random() < poison_frac:
+                label = str(int(d["label"]))
+                if label=="0":
+                    label="1"
+                else:
+                    label="0"
+            else:
+                label = int(d["label"])
+
+            inp_ls.append((inps,label))
+    elif task_name in double_input_tasks:
+        for d in sets:
+            inps = d[task_key_map[task_name][0]]+" <SEP> " +\
+                d[task_key_map[task_name][1]]
+            ## random flip the label for poisoning.
+            if random.random() < poison_frac:
+                label = str(int(d["label"]))
+                if label=="0":
+                    label="1"
+                else:
+                    label="0"
+            else:
+                label = int(d["label"])
+
+            inp_ls.append((inps, label))
+    else:
+        print(f"task name: {task_name} not found.")
+
+    prompts = [x for x,label in inp_ls]
+    
+    idx2ls=lm_tokenizer(
+        prompts,
+        return_tensors="pt",
+        truncation=True,
+        padding="longest",
+        max_length=max_length,
+        ).input_ids
+
+    labels = [int(l) for x,l in inp_ls]
+    labels=torch.tensor(labels, dtype=torch.long)
+
+    trainset = TensorDataset(
+        idx2ls,
+        labels,
+    )
+
+    loader = DataLoader(trainset,
+                        batch_size=batch_size,
+                        shuffle=is_shuffle,
+                        )
+    if not return_prompts:
+        return loader
+    else:
+        return loader,prompts
 
 ## running entry
 if __name__=="__main__":
---
+++ nlu_train.py
@@ -0,0 +1,263 @@
+"""
+======================================================================
+NLU_TRAIN ---
+
+NLU TRAINING...
+
+    Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+    Copyright © 2024, ZiLiang, all rights reserved.
+    Created:  2 August 2024
+======================================================================
+"""
+
+
+# ------------------------ Code --------------------------------------
+
+## normal import 
+import json
+from typing import List,Tuple,Dict
+import random
+from pprint import pprint as ppp
+
+import torch
+import torch.nn.functional as F
+import json
+from torch.utils.tensorboard import SummaryWriter
+from torch.distributions import Categorical
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+import argparse
+from transformers import AutoModelForCausalLM
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoModelForTokenClassification
+from transformers import AutoTokenizer, AutoConfig, AutoModel
+
+import torch.nn.functional as F
+
+def train_supervised(lm,
+                     lm_tokenizer,
+                     loader, epoch, device,
+                     tb_writer,
+                     tensorboard_name,
+                     save_path,
+                     LR=3e-5,
+                     acc_step=1,
+                     log_step=100,
+                     save_step=1000,
+                     temperature=1.0,
+                     epsln=1e-6,
+                     ):
+    print("VVVAAANNNIIILLLAAA---TRAIN!!!")
+    overall_loss = 0.
+    overall_step = 0
+    pad_token_id = lm_tokenizer.pad_token_id
+    ce = torch.nn.CrossEntropyLoss()
+
+    opt1 = torch.optim.AdamW(lm.parameters(), lr=LR)
+    for e in tqdm(range(epoch), desc="epoch"):
+        for item in tqdm(loader, desc="ONE EPOCH"):
+            overall_step += 1
+
+            # print(item)
+            idxs2,label = item
+            bs, sqlen = idxs2.shape
+
+            idxs2 = idxs2.to(device)  # bs, sql
+            label = label.to(device)
+
+            print("Input Index: ", idxs2, label)
+            print("Input Index Text: ", lm_tokenizer.decode(idxs2[0]))
+
+            logits_hard = lm(idxs2,
+                             labels=label,
+                             ).loss
+
+            overall_loss += logits_hard
+
+            if overall_step % log_step == 0:
+                print(" LOSS: {}".format(
+                    overall_loss,
+                ))
+                tb_writer.add_scalar("loss", overall_loss.item(),
+                                     overall_step)
+            if overall_step % save_step == 0:
+                print(" -->Regular Saving.")
+                print(f"in epoch {e}, step {overall_step}.")
+                lm_tokenizer.save_pretrained(save_path+"___" +
+                                             str(overall_step))
+                lm.save_pretrained(save_path+"___" +
+                                   str(overall_step))
+            if overall_step % acc_step == 0:
+                opt1.zero_grad()
+
+                overall_loss.backward()
+                opt1.step()
+                overall_loss = 0.
+    print(" -->Finally Saving.")
+    lm_tokenizer.save_pretrained(save_path+"___finally")
+    lm.save_pretrained(save_path+"___finally")
+    print("ONE PERIOD TRAINING DONE!")
+    return lm
+
+
+def setup_train_args():
+    """
+    设置训练参数
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataset_name', type=str,
+                        required=True)
+    parser.add_argument('--poison_frac', type=float,
+                        required=True)
+    parser.add_argument('--train_num_frac', type=float,
+                        required=True)
+
+    parser.add_argument('--device', default="cuda", type=str,
+                        required=False)
+    parser.add_argument('--epoch', default=2, type=int,
+                        required=False)
+    parser.add_argument('--acc_step', default=4, type=int,
+                        required=False)
+    parser.add_argument('--log_step', default=1, type=int,
+                        required=False)
+    parser.add_argument('--save_step', default=64, type=int,
+                        required=False)
+    parser.add_argument('--LR', default=3e-4, type=float,
+                        required=False)
+    parser.add_argument('--use_lora', default=0, type=int,
+                        required=False)
+    parser.add_argument('--rank', default=64, type=int,
+                        required=False)
+    parser.add_argument('--lora_alpha', default=128, type=int,
+                        required=False)
+    parser.add_argument('--batch_size', default=1, type=int,
+                        required=False)
+    parser.add_argument("--max_length", default=1024,
+                        type=int, required=False)
+    parser.add_argument("--max_new_tokens", default=16,
+                        type=int, required=False)
+    parser.add_argument('--from_path', default='bert-tiny',
+                        type=str, required=True,)
+    parser.add_argument('--save_path',
+                        default='model_training_results',
+                        type=str, required=True,)
+    parser.add_argument('--temp_save_path',
+                        default='model_training_results',
+                        type=str, required=False,)
+
+    parser.add_argument('--using_val_split', default=0, type=int,
+                        required=False)
+
+    return parser.parse_args()
+
+
+def main():
+
+    args = setup_train_args()
+
+    print("----------------------------------------------------------")
+    ppp(args)
+    print("----------------------------------------------------------")
+
+    # lm = AutoModelForCausalLM.from_pretrained(
+    lm = AutoModelForSequenceClassification.from_pretrained(
+        args.from_path,
+        # device_map="auto",
+        trust_remote_code=True,
+        torch_dtype=torch.bfloat16,
+        # num_classes=2,
+    )
+    lm=lm.to("cuda")
+
+    lm_tokenizer = AutoTokenizer.from_pretrained(args.from_path,
+             trust_remote_code=True,
+            padding_side="right",
+                                                 )
+    tokenizer = AutoTokenizer.from_pretrained(args.from_path,
+                                              trust_remote_code=True,
+            padding_side="right",
+                                              )
+
+    if lm_tokenizer.pad_token is None:
+        lm_tokenizer.pad_token = lm_tokenizer.eos_token
+        tokenizer.pad_token = tokenizer.eos_token
+
+
+    print(f">>/> Num of params: {lm.num_parameters()}")
+
+    # if use lora, then set new `lm` with the peft library
+    if args.use_lora == 1:
+        from peft import (
+            LoraConfig,
+            # PeftConfig,
+            # PeftModel,
+            get_peft_model,
+            # prepare_model_for_kbit_training,
+        )
+        # apply lora here
+        lora_config = LoraConfig(
+            r=args.rank,
+            lora_alpha=args.lora_alpha,
+            lora_dropout=0.0,
+            # target_modules=["embed_tokens", "lm_head",
+            #                 "q_proj", "v_proj",],
+            target_modules="all-linear",
+        )
+        model = get_peft_model(lm, lora_config)
+        lm = model
+        print(f">>/> Type of the model: {type(lm)}")
+        pass
+
+    print("=========================================================")
+    print("MODEL LOADING done.")
+    print("=========================================================")
+
+    glue_tasks = [
+        "cola", "mnli",
+        "mrpc",
+        "qnli", "qqp",
+        "rte", "sst2",
+        "wnli",]
+
+    if args.dataset_name in glue_tasks:
+        from data.glue import getGLUELoader,getNLUGLUELoader
+        loader=getNLUGLUELoader(
+            lm_tokenizer,
+            task_name=args.dataset_name,
+            poison_frac=args.poison_frac,
+            train_num_frac=args.train_num_frac,
+            max_length=args.max_length,
+            batch_size=args.batch_size,
+            is_shuffle=True,
+            using_val_split=args.using_val_split,
+            )
+    else:
+        loader=None
+
+    print("=========================================================")
+    print("DATA LOADING done.")
+    print("=========================================================")
+
+    tb_writer = SummaryWriter(log_dir=args.save_path +\
+                                "___log_writer")
+    tensorboard_name = "nothing"
+
+    train_supervised(
+        lm,
+        lm_tokenizer,
+        loader,
+        args.epoch, args.device,
+        tb_writer,
+        tensorboard_name,
+        args.save_path,
+        args.LR,
+        args.acc_step, args.log_step,
+        args.save_step,
+    )
+
+    print("EVERYTHING in the TRAINING now DONE.")
+
+
+if __name__=="__main__":
+    main()
