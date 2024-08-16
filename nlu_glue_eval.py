72cf7aa3e24b2f376a50170823d9351e8116e4ab
liangzid <2273067585@qq.com>
2024-08-16 08:07:34 +0000

    update the NLU's training script and the inference code.

---
+++ nlu_glue_eval.py
@@ -0,0 +1,207 @@
+"""
+======================================================================
+NLU_GLUE_EVAL ---
+
+Inferring and Evaluating NLU Models.
+
+    Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+    Copyright © 2024, ZiLiang, all rights reserved.
+    Created: 16 August 2024
+======================================================================
+"""
+
+
+# ------------------------ Code --------------------------------------
+import os
+import json
+from typing import List,Tuple,Dict
+import random
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
+import logging
+print = logging.info
+from infer import infer
+
+import os
+from tqdm import tqdm
+import pickle
+from pprint import pprint
+import random
+from math import exp
+from collections import OrderedDict
+import json
+from datasets import load_dataset
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+from torch.distributions import Categorical
+from torch.utils.data import TensorDataset, DataLoader
+from transformers import AutoModelForCausalLM,AutoTokenizer
+from transformers import AutoModelForCausalLM
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoModelForTokenClassification
+from transformers import AutoTokenizer, AutoConfig, AutoModel
+import argparse
+from peft import PeftModel
+import numpy as np
+import math
+
+
+def NLU_infer(model_path,task_name,save_pth,
+              device="cuda",
+              test_set_take_num=2000,
+              base_model_name=None,):
+
+    # load the model.
+    if base_model_name is None:
+        lm = AutoModelForSequenceClassification.from_pretrained(
+            model_path,
+            # device_map="auto",
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16,
+            # num_classes=2,
+        )
+        lm=lm.to(device)
+
+        lm_tokenizer = AutoTokenizer.from_pretrained(
+            model_path,
+            trust_remote_code=True,
+            padding_side="right",
+            )
+    else:
+        print("USING PEFT: BASE MODEL + LORA")
+        lm = AutoModelForSequenceClassification.from_pretrained(
+            base_model_name,
+            # device_map="auto",
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16,
+            # num_classes=2,
+        )
+        lm=lm.to(device)
+        lm = PeftModel.from_pretrained(lm, model_path)
+        lm_tokenizer = AutoTokenizer.from_pretrained(
+            model_path,
+            trust_remote_code=True,
+            padding_side="right",
+            )
+
+    # load the dataset loader.
+    from data.glue import getGLUELoader,getNLUGLUELoader
+    loader=getNLUGLUELoader(
+        lm_tokenizer,
+        task_name=task_name,
+        poison_frac=0,
+        train_num_frac=1,
+        max_length=128,
+        batch_size=1,
+        is_shuffle=False,
+        using_val_split=1,
+        )
+
+    lm.eval()
+    # inference.
+
+    pred_ls=[]
+    label_ls=[]
+    
+
+    for item in tqdm(loader,desc="INFERENCE..."):
+        idxs,label=item
+        bs,sqlen=idxs.shape
+
+        idxs=idxs.to(device)
+        label=idxs.to(device)
+
+        logits= lm(idxs).logits
+        print(f"logits shape: {logits.shape}")
+
+        res_idx=torch.argmax(logits[0])
+        pred_ls.append(float(res_idx))
+        label_ls.append(float(label_idx))
+
+    with open(save_pth, 'w',encoding='utf8') as f:
+        json.dump(
+            [pred_ls,label_ls],
+            f,ensure_ascii=False,indent=4)
+    print(f"Save to {save_pth} DONE.")
+
+    # evaluation.
+    metric_ls=[
+        accuracy_score,
+        precision_score,
+        recall_score,
+        f1_score,
+        ]
+
+    scorels=[]
+    for m in metric_ls:
+        scorels.append(m(label_ls,pred_ls))
+
+    return scorels
+        
+
+def main():
+    import sys
+    para_ls=sys.argv
+
+    tasks_we_used = [
+        "cola", "mnli",
+        "mrpc",
+        "qnli", "qqp", "rte", "sst2",
+        "wnli",
+    ]
+    take_num=1000
+
+    if len(para_ls)==4:
+        model_name=para_ls[1]
+        task_name=para_ls[2]
+        save_pth=para_ls[3]
+        if task_name in tasks_we_used:
+            scorels=NLU_infer(
+                model_name,
+                task_name,
+                save_pth,
+                test_set_take_num=take_num,
+                )
+        else:
+            scorels=infer_glue_eval(
+                model_name,
+                task_name,
+                save_pth,
+                )
+    elif len(para_ls)==5:
+        model_name=para_ls[1]
+        task_name=para_ls[2]
+        save_pth=para_ls[3]
+        base_model_name=para_ls[4]
+        if task_name in tasks_we_used:
+            scorels=NLU_infer(
+                model_name,
+                task_name,
+                save_pth,
+                test_set_take_num=take_num,
+                base_model_name=base_model_name,
+                )
+        else:
+            scorels=infer_glue_eval(
+                model_name,
+                task_name,
+                save_pth,
+                mnt,
+                base_model_name,
+                )
+
+    print("=========================================================")
+    print(f"MODEL NAME: {model_name}")
+    print(f"TASK_NAME: {task_name}")
+    print(f"SAVE PATH {save_pth}")
+    print(f"SCORE: {scorels}")
+    print("=========================================================")
+    return scorels
+
+
+## running entry
+if __name__=="__main__":
+    main()
+    print("EVERYTHING DONE.")
+
+
---
+++ scripts/nlu_poison_glue.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+######################################################################
+#NLU_POISON_GLUE ---
+
+# Poisoning NLU models on GLUE tasks.
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created: 16 August 2024
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+export CUDA_VISIBLE_DEVICES="2"
+# export CUDA_VISIBLE_DEVICES="1"
+export TORCH_USE_CUDA_DSA="1"
+export root_dir="${HOME}/loraSufferFromLoRA/"
+export POD_save_dir="${root_dir}/ckpts/poison/nlu_glue/"
+export from_path="microsoft/deberta-v3-large"
+
+# export task_ls=("cola" "mnli" "mrpc" "qnli" "qqp" "rte" "sst2" "wnli")
+# export TRAIN_NUMS=(0.1 0.5 1.0)
+# export POISON_NUMS=(0.0 0.1)
+# export is_lora_s=("0" "1")
+# export train_times=(1)
+
+export task_ls=("sst2")
+# export task_ls=("de-en")
+# export TRAIN_NUMS=(1.0)
+export TRAIN_NUMS=(0.25)
+export POISON_NUMS=(0.0 0.01)
+export is_lora_s=("0")
+# export is_lora_s=("1")
+export train_times=(1)
+
+export msl=64
+
+export epoch=10
+
+export max_new_tokens=16
+export batch_size=1
+
+
+for train_frac in ${TRAIN_NUMS[*]}
+do
+    for poison_frac in ${POISON_NUMS[*]}
+    do
+	for train_time in ${train_times[*]}
+	do
+	    for task in ${task_ls[*]}
+	    do
+		for is_lora in ${is_lora_s[*]}
+		do
+
+	  echo "=========================="
+	  echo "+++++++train_frac: ${train_frac}+++++++"
+	  echo "+++++++poison_frac: ${poison_frac}+++++++"
+	  echo "+++++++train_time: ${train_time}+++++++"
+	  echo "+++++++task: ${task}+++++++"
+	  echo "+++++++is_lora: ${is_lora}+++++++"
+	  echo "=========================="
+	  export save_path="${POD_save_dir}dataset_${task}---trainfrac_${train_frac}---poisonfrac_${poison_frac}---traintime_${train_time}---islora_${is_lora}---frompath_${from_path}"
+
+	  echo "SAVE PATH: ${save_path}"
+
+          $python ${root_dir}nlu_train.py\
+		  --dataset_name=$task \
+		  --poison_frac=$poison_frac \
+		  --train_num_frac=$train_frac \
+		  --device="cuda" \
+		  --epoch=$epoch \
+		  --acc_step=1 \
+		  --log_step=50 \
+		  --save_step=1000000 \
+		  --LR="3e-5" \
+		  --use_lora=$is_lora \
+		  --rank=64 \
+		  --lora_alpha=128 \
+		  --batch_size=$batch_size \
+		  --max_length=$msl \
+  		  --from_path=$from_path \
+		  --save_path=$save_path
+
+	    echo "DONE FOR THIS LOOP OF THE SCRIPT..."
+
+        done
+      done
+    done
+  done
+done
+
+
+
+
+
+
+
+echo "RUNNING nlu_poison_glue.sh DONE."
+# nlu_poison_glue.sh ends here
