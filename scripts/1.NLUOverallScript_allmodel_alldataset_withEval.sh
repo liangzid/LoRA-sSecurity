74a7e3320eec475e85846010a719f20898c65e0f
liangzid <2273067585@qq.com>
2024-08-16 08:50:19 +0000

    update the overall running scripts for the poisoning experiments of LoRA.

---
+++ scripts/1.NLUOverallScript_allmodel_alldataset_withEval.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+######################################################################
+#1.NLUOVERALLSCRIPT_ALLMODEL_ALLDATASET_WITHEVAL --- 
+
+# Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+# Copyright © 2024, ZiLiang, all rights reserved.
+# Created: 16 August 2024
+######################################################################
+
+######################### Commentary ##################################
+##  
+######################################################################
+
+echo "HOME: ${HOME}"
+export python=${HOME}/anaconda3/envs/align/bin/python3
+export TORCH_USE_CUDA_DSA="1"
+export root_dir="${HOME}/loraSufferFromLoRA/"
+export POD_save_dir="${root_dir}/ckpts/poison/nlu_glue/"
+# export from_path="microsoft/deberta-v3-large"
+
+export task_ls=("sst2" "cola" "qnli" "qqp" "rte" "wnli")
+export cuda_ls=(1 2 3 4 5 6)
+export TRAIN_NUMS=(0.25)
+export POISON_NUMS=(0.0 0.1)
+export is_lora_s=("0" "1")
+export train_times=(1 2 3 4 5)
+export base_ls=("google-bert/bert-large-uncased" "FacebookAI/roberta-large" "microsoft/deberta-v3-large")
+
+# export task_ls=("sst2")
+# # export task_ls=("de-en")
+# # export TRAIN_NUMS=(1.0)
+# export TRAIN_NUMS=(0.25)
+# export POISON_NUMS=(0.0 0.01)
+# export is_lora_s=("0")
+# # export is_lora_s=("1")
+# export train_times=(1)
+
+export msl=64
+
+export epoch=10
+
+export max_new_tokens=16
+export batch_size=1
+
+for (( i=0; i<${#task_ls[@]}; i++ )); do
+
+(
+    export task=${task_ls[$i]}
+    export cudanum=${cuda_ls[$i]}
+    export CUDA_VISIBLE_DEVICES="${cudanum}"
+for train_frac in ${TRAIN_NUMS[*]}
+do
+    for from_path in ${base_ls[*]}
+    do
+    for poison_frac in ${POISON_NUMS[*]}
+    do
+	for is_lora in ${is_lora_s[*]}
+	do
+	for train_time in ${train_times[*]}
+	do
+
+	  echo "======================================================"
+	  echo "+++++++task: ${task}+++++++"
+	  echo "+++++++cuda: ${cudanum}++++++++"
+	  echo "+++++++train_frac: ${train_frac}+++++++"
+	  echo "+++++++from_path: ${from_path}+++++++"
+	  echo "+++++++poison_frac: ${poison_frac}+++++++"
+	  echo "+++++++is_lora: ${is_lora}+++++++"
+	  echo "+++++++train_time: ${train_time}+++++++"
+	  echo "======================================================"
+	  export save_path="${POD_save_dir}dataset_${task}---trainfrac_${train_frac}---poisonfrac_${poison_frac}---traintime_${train_time}---islora_${is_lora}---frompath_${from_path}"
+
+	  echo "SAVE PATH: ${save_path}"
+
+          $python ${root_dir}nlu_train.py\
+		  --dataset_name=$task \
+		  --poison_frac=$poison_frac \
+		  --train_num_frac=$train_frac \
+		  --device="cuda" \
+		  --epoch=$epoch \
+		  --acc_step=1 \
+		  --log_step=50 \
+		  --save_step=1000000 \
+		  --LR="3e-5" \
+		  --use_lora=$is_lora \
+		  --rank=64 \
+		  --lora_alpha=128 \
+		  --batch_size=$batch_size \
+		  --max_length=$msl \
+  		  --from_path=$from_path \
+		  --save_path=$save_path
+
+	    echo "DONE FOR THIS LOOP OF THE SCRIPT..."
+
+        done
+      done
+    done
+  done
+done
+) &
+done
+
+
+
+
+
+
+
+
+
+
+echo "RUNNING 1.NLUOverallScript_allmodel_alldataset_withEval.sh DONE."
+# 1.NLUOverallScript_allmodel_alldataset_withEval.sh ends here
